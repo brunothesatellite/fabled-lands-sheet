4f9b3bf8a3e0ad87e77ef72111a1ca9490f58354
brunothesatellite <40961631+brunothesatellite@users.noreply.github.com>
2026-09-10 22:18:13 +0200

    ajout de scripts pour test home

---
+++ deploy/starthome.bat
@@ -0,0 +1,6 @@
+@echo off
+title Fabled Lands - Local Server
+echo Demarrage du serveur local...
+echo.
+"C:\Users\Bruno\AppData\Local\Programs\Python\Python311\python.exe" "%~dp0starthome.py"
+pause
